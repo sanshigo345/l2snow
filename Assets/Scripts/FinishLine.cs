640b609504d80278bdf2305c381214bcfdcdd00b
bugra <bugrasshgercan@gmail.com>
2022-06-22 16:59:28 +0300

    added 38 triggers to restart level, win/lose

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other) {
+
+        if(other.tag == "Player") {
+            Debug.Log("you won!");
+        }
+        
+    }
+}
