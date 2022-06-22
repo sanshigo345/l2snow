8809065212dedf5cfddfab1718e020663480b2d6
bugra <bugrasshgercan@gmail.com>
2022-06-22 17:50:23 +0300

    added 40. Using invoke for delays

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 0.4f;
     private void OnTriggerEnter2D(Collider2D other) {
         
         if (other.tag == "Ground") {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+        void ReloadScene() {
+        SceneManager.LoadScene(0);
+
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,12 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float reloadDelay = 1f;
     private void OnTriggerEnter2D(Collider2D other) {
 
         if(other.tag == "Player") {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }
         
     }
+
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
+
+    }
+
 }
